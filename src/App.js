5fa920f141729c8bdfe1c4fcf9336ce59f884053
Vincent Vuram <vincentvuram204@gmail.com>
2023-04-29 20:07:56 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
-import { createContext, useState } from 'react' 
+import { createContext } from 'react' 
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import { useQuery } from 'react-query'
 import { fetchUsers } from './api/api'
-import UserPage from './pages/UserPage'
-import UserPosts from './pages/UserPosts'
-import UserDetails from './pages/UserDetails'
-import UserGallery from './pages/UserGallery'
-import UserTodo from './pages/UserTodo'
+import UserPage from './pages/UserPage' 
 
 export const UserContext = createContext()
 
-function App() {
-  const [users, setUsers] = useState([])
-  const {data, isLoading, isFetching, isFetched, isError} = useQuery('users',fetchUsers)
+function App() { 
+  const {data, isLoading,   isError} = useQuery('users',fetchUsers)
 
-  const routes = [
-    { 
-        route:'detail',
-        element:<><UserDetails/></>
-    },
-    { 
-        route:'posts',
-        element:<><UserPosts/></>
-    },
-    { 
-        route:'gallery',
-        element:<><UserGallery/></>
-    },
-    { 
-        route:'todo',
-        element:<><UserTodo/></>
-    },
-
-]
+ 
 
   if(isLoading) return <>Loading...</> 
   if(isError) return <>Error</> 
